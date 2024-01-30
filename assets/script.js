74ca64de00a571c104c10d05b079091fda160ec0
Demitri <nunziatademitri@yahoo.co.uk>
2024-01-30 19:07:41 +0000

    api key generated and working
    api key generated and functioning correclty.

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const cityInput = document.querySelector("#city-input");
+const searchButton = document.querySelector("#search-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const daysForecastDiv = document.querySelector(".days-forecast");
+
+const API_KEY = "3ea37f3385002188f2615e5641d72d65"; // 
